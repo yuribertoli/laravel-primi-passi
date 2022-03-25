edce4a6c4364c13a3dd05b9e8b60fe0a82a41094
yuribertoli <yuribertoli@yahoo.it>
2022-03-25 15:01:49 +0100

    testo

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+    $data = [
+        "testo"=>"non so cosa scrivere"
+    ];
+
+    return view('home', $data);
 });
